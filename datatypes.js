e4d218ec45059abaacfcb9fe6e9e623722615246
Tooma81 <toomas.kaarel@gmail.com>
2023-10-03 12:15:14 +0300

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+// data types
+// String
+let first_name = "Toomas Kaarel"
+let last_name = "Elmet"
+console.log(first_name + " " + last_name)
+
+//Numbers
+let goodmark = 5
+console.log(typeof goodmark)
+
+//Boolean
+let isToday = true
+console.log(typeof isToday)
+
+//type conversions
+console.log(typeof String(73829))
+console.log(String(738 + 29))
+
+//boolean --> String
+console.log(typeof String(true))
+console.log(parseInt(10.7))
+console.log(parseFloat(10.7))
